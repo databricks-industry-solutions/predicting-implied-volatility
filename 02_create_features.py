cfebc56be1e2ca5d4a04d3aa46cacb07e13e0c56
dbbnicole <dbbnicole>
2023-05-09 16:05:43 +0000

    Fix tfp version

--- 02_create_features.py
+++ 02_create_features.py
@@ -1,5 +1,5 @@
 # Databricks notebook source
-# MAGIC %pip install tensorflow==2.11 tensorflow-probability tf_quant_finance
+# MAGIC %pip install tensorflow==2.11 tensorflow-probability==0.19.0 tf_quant_finance
 
 # COMMAND ----------
 
@@ -23,11 +23,11 @@ import pyspark.pandas as ps
 # MAGIC %md
 # MAGIC # Step 1. Model Setup
 # MAGIC Let's start by defining a *toy model* (generic Ito Process), which will be function of specific model parameters. 
-# MAGIC 
+# MAGIC
 # MAGIC This model can be used to price call options with specific *maturities* and *strikes* and therefore implied vols (using blackscholes one to one mapping between prie and implied vol).
-# MAGIC 
+# MAGIC
 # MAGIC Our aim find the values of model paramters such that implied vols calculated from *this* model matches with implied vols obtained from market (this process is called calibration).
-# MAGIC 
+# MAGIC
 # MAGIC ## 1.1. Model Definition
 # MAGIC Creating a toy model definition by following [lognormal](https://en.wikipedia.org/wiki/Log-normal_distributio) fx, [vasicek](https://en.wikipedia.org/wiki/Vasicek_model) ir & [local vol](https://en.wikipedia.org/wiki/Local_volatility) fx_vol.
 
@@ -197,7 +197,7 @@ class BlackScholesWithVasicelAndLocalVol(GenericItoProcess):
 
 # MAGIC %md
 # MAGIC ## 1.2. Model Initialization
-# MAGIC 
+# MAGIC
 # MAGIC Initialize the model with dummy values of model parameters
 
 # COMMAND ----------
@@ -244,9 +244,9 @@ model = BlackScholesWithVasicelAndLocalVol(
 
 # MAGIC %md
 # MAGIC ## 1.3. Model Calibration
-# MAGIC 
+# MAGIC
 # MAGIC To keep things simple let's just try to find the *lv_surface* that will map closest match to our target i.e implied_vol from market.
-# MAGIC 
+# MAGIC
 # MAGIC Moment of truth: Given the input dimenion of objective function is 50 ('local_vol_fx' surface of size 10x5 i.e 10 maturities by 5 strikes) and similarly output dimension is 50 as well, below cell execution will take forever. (if one really wants to test the execution, can reduce *option_maturities* to size of 1 array)
 
 # COMMAND ----------
@@ -320,11 +320,11 @@ durr_ = end_time_ - start_time_
 
 # MAGIC %md
 # MAGIC # Step 2: Use Machine learning to reduce calibration time
-# MAGIC 
+# MAGIC
 # MAGIC Now we should try to reduce calibration time complexity using machine learning. The major bottleneck in above calibration is repeated calls to *model.implied_vol* function inside optimizer which is quite heavy due to Monte-Carlo Simulation.
-# MAGIC 
+# MAGIC
 # MAGIC If somehow we can learn that function (which is mapping local_vol model parameter to implied vol), then we can use that function underneath the optimizer as replacement and it would be many fold faster!
-# MAGIC 
+# MAGIC
 # MAGIC ## 2.1. Training Data Generation
 # MAGIC First step is generate training dataset
 
@@ -381,11 +381,11 @@ assert len(schema_) == features.shape[1] == labels.shape[1], 'There is a mismatc
 # COMMAND ----------
 
 # MAGIC %md 
-# MAGIC 
+# MAGIC
 # MAGIC ## 2.2. Convert the features and labels into Koalas DataFrames
-# MAGIC 
+# MAGIC
 # MAGIC Koalas provides a drop-in replacement for pandas. Commonly used by data scientists, pandas is a Python package that provides easy-to-use data structures and data analysis tools for the Python programming language. However, pandas does not scale out to big data. Koalas fills this gap by providing pandas equivalent APIs that work on Apache Spark. Koalas is useful not only for pandas users but also PySpark users, because Koalas supports many tasks that are difficult to do with PySpark, for example plotting data directly from a PySpark DataFrame.
-# MAGIC 
+# MAGIC
 # MAGIC https://docs.databricks.com/languages/koalas.html
 
 # COMMAND ----------
@@ -401,16 +401,16 @@ labels_ps = ps.DataFrame(labels, columns=schema_).reset_index()
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ## Why use Databricks Feature Store?
 # MAGIC Databricks Feature Store is fully integrated with other components of Databricks.
-# MAGIC 
+# MAGIC
 # MAGIC - **Lineage**. When you create a feature table with Databricks Feature Store, the data sources used to create the feature table are saved and accessible. For each feature in a feature table, you can also access the models, notebooks, jobs, and endpoints that use the feature.
-# MAGIC 
+# MAGIC
 # MAGIC - **Discoverability**. The Databricks Feature Store UI, accessible from the Databricks workspace, lets you browse and search for existing features.
-# MAGIC 
+# MAGIC
 # MAGIC - **Integration** with model scoring and serving. When you use features from Databricks Feature Store to train a model, the model is packaged with feature metadata. When you use the model for batch scoring or online inference, it automatically retrieves features from Feature Store. The caller does not need to know about them or include logic to look up or join features to score new data. This makes model deployment and updates much easier.
-# MAGIC 
+# MAGIC
 # MAGIC https://docs.databricks.com/applications/machine-learning/feature-store/index.html
 
 # COMMAND ----------
@@ -421,7 +421,7 @@ fs = feature_store.FeatureStoreClient()
 # COMMAND ----------
 
 # MAGIC %sql 
-# MAGIC 
+# MAGIC
 # MAGIC -- Create the Feature Store database
 # MAGIC CREATE DATABASE IF NOT EXISTS feature_store_implied_volatility; 
 
@@ -444,21 +444,21 @@ fs.create_table(
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ## 3.1. Display the generated data and check the distribution
 
 # COMMAND ----------
 
 # MAGIC %sql 
-# MAGIC 
+# MAGIC
 # MAGIC use feature_store_implied_volatility
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ### 3.1.1. Visualise the generated data and explore
-# MAGIC 
+# MAGIC
 # MAGIC Databricks Notebooks have built-in dashboarding capabilities (which we can observe below). We can very quickly visualise the features and labels that we just saved into the Databricks Feature Store. Below we have the scattered plot of various strike levels for the same maturity (of the generated data). The chart even offers a LOESS regression that can give us even more information about the distribution of the residuals of the generated data.
 
 # COMMAND ----------
@@ -468,9 +468,9 @@ display(spark.sql('select * from labels'))
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC ### 3.1.2. Data Profiling
-# MAGIC 
+# MAGIC
 # MAGIC Databricks Notebooks have built-in data profiling features. In the cell below, we can observe a lot of statistical information for the newly generated features and labels, without having to use third-party tools or write additional code.
 
 # COMMAND ----------
@@ -480,9 +480,9 @@ display(spark.sql('select * from labels'))
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC # Step 4: Check the generated data for statistical issues using R
-# MAGIC 
+# MAGIC
 # MAGIC We will use R libraries to automatically test the newly generated data for heteroskedasticity.
 
 # COMMAND ----------
@@ -492,7 +492,7 @@ features_ps.iloc[:, 1:].to_spark().createOrReplaceTempView('IVfeatures_view')
 # COMMAND ----------
 
 # MAGIC %r
-# MAGIC 
+# MAGIC
 # MAGIC library(SparkR)
 # MAGIC sql("REFRESH TABLE IVfeatures_view")
 # MAGIC features_df_r <- sql("SELECT * FROM IVfeatures_view")
@@ -500,15 +500,15 @@ features_ps.iloc[:, 1:].to_spark().createOrReplaceTempView('IVfeatures_view')
 # COMMAND ----------
 
 # MAGIC %r
-# MAGIC 
+# MAGIC
 # MAGIC head(features_df_r)
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC 
+# MAGIC
 # MAGIC # Step 5: Train ML model for each of the labels 
-# MAGIC 
+# MAGIC
 # MAGIC See **the next notebook in this solution accelerators**
 
 # COMMAND ----------
